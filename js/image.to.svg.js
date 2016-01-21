65db5c0a58c04d5c6035f62aed7919875c640ba3
Aleksandr Nikitin <a.nikitin@i.ua>
2016-01-21 19:43:21 +0200

    Add link and popolnenie-scheta page

--- js/image.to.svg.js
+++ js/image.to.svg.js
@@ -1,23 +1,25 @@
 (function(){
-	$('img.cl-icon-svg').each(function(){
-		var $img = $(this);
-		if ($img.hasClass('cl-icon-set-color')) {
-			var imgURL = $img.attr('src');
-			var imgWidth = $img.attr('width');
-			var imgHeight = $img.attr('height');
-			var imgColor = $img.attr('data-replace-color');
+	$(function(){
+		$('img.cl-icon-svg').each(function(){
+			var $img = $(this);
+			if ($img.hasClass('cl-icon-set-color')) {
+				var imgURL = $img.attr('src');
+				var imgWidth = $img.attr('width');
+				var imgHeight = $img.attr('height');
+				var imgColor = $img.attr('data-replace-color');
 
-			$.get(imgURL, function (data) {
-				var $svg = $(data).find('svg');
-				var $style = $svg.find('style');
-				var style = $style.text();
+				$.get(imgURL, function (data) {
+					var $svg = $(data).find('svg');
+					var $style = $svg.find('style');
+					var style = $style.text();
 
-				style = style.replace(/#....../gi, '#'+imgColor);
-				$style.text(style);
-				$svg.removeAttr('xmlns:a').attr({width: imgWidth, height: imgHeight});
+					style = style.replace(/#....../gi, '#'+imgColor);
+					$style.text(style);
+					$svg.removeAttr('xmlns:a').attr({width: imgWidth, height: imgHeight});
 
-				$img.replaceWith($svg);
-			}, 'xml');
-		}
+					$img.replaceWith($svg);
+				}, 'xml');
+			}
+		});
 	});
 })();
