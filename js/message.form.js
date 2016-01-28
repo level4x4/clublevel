fec9febf2f09c6f6acff1f298286080c84559e5d
Aleksandr Nikitin <a.nikitin@i.ua>
2016-01-28 10:49:51 +0200

    Fixed message js

--- js/message.form.js
+++ js/message.form.js
@@ -6,16 +6,16 @@
 
 	var renderPopup = function() {
 		$mfPopupWindow.append(
-			$('<form/>').append(
+			$('<form/>', {action: '', method: 'POST'}).append(
 				$('<p/>', {class: 'mf-title-popup', text: 'Написать сообщение'})
 			).append(
-				$('<input/>', {class: 'mf-popup-input', placeholder: 'Имя'})
+				$('<input/>', {class: 'mf-popup-input', type: 'text', placeholder: 'Имя', required: true})
 			).append(
-				$('<input/>', {class: 'mf-popup-input', placeholder: 'Контакт (телефон, e-mail)'})
+				$('<input/>', {class: 'mf-popup-input', type: 'text', placeholder: 'Контакт (телефон, e-mail)', required: true})
 			).append(
-				$('<textarea/>', {class: 'mf-popup-textarea', placeholder: 'Текст сообщения'})
+				$('<textarea/>', {class: 'mf-popup-textarea', placeholder: 'Текст сообщения', required: true})
 			).append(
-				$('<button/>', {class: 'btn btn-big btn-text', text: 'Отправить'})
+				$('<button/>', {class: 'btn btn-big btn-text mf-submit', type: 'submit', text: 'Отправить'})
 			).append(
 				$('<i/>', {class: 'mf-close'})
 			)
@@ -36,5 +36,15 @@
 	$(document).on('click', '.mf-close', function(){
 		$_mfPopupContainer.remove();
 		return false;
+	});
+
+	$(document).on('click', '.mf-submit', function(){
+		var $mfPopupInput = $('.mf-popup-input'),
+			$mfPopupTextarea = $('.mf-popup-textarea');
+		var $input1 = $($mfPopupInput[0]);
+		var $input2 = $($mfPopupInput[1]);
+		if ($input1.val() && $input2.val() && $mfPopupTextarea.text()) {
+			$_mfPopupContainer.remove();
+		}
 	});
 })();
